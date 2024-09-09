8a11f3a81528e407a5c1477bc7264e7e5bea893b
Shane Barton <shanecb@hotmail.ca>
2024-09-09 15:17:40 -0400

    Add directional movement with new unity input system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed;
+
+    private PlayerControls playerControls;
+    private Animator animator;
+    private Rigidbody2D rb;
+    private Vector2 movementVector;
+
+    private void Awake()
     {
-        
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerControls();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    /// <summary>
+    /// This function polls the input action map for player input and sets it within the animator.
+    /// </summary>
+    private void PlayerInput()
+    {
+        movementVector = playerControls.Movement.Move.ReadValue<Vector2>();
+        animator.SetFloat("MoveX", movementVector.x);
+        animator.SetFloat("MoveY", movementVector.y);
+    }
+
+    private void MovePlayer()
+    {
+        Vector2 position = rb.position + movementVector * (moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(position);
     }
 }
