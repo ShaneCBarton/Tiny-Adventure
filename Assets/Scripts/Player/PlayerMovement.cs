f1edfa468fba1decd631dc8d97d3b85cdc99e76f
Shane Barton <shanecb@hotmail.ca>
2024-12-04 13:10:37 -0500

    Add footstep sounds. Add logic to play sounds on different terrain.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -10,6 +10,15 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private LayerMask waterLayer;
     [SerializeField] private LayerMask groundLayer;
 
+    [Header("Audio")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip[] landFootsteps;
+    [SerializeField] private AudioClip[] waterFootsteps;
+    [SerializeField] private float footstepInterval = 0.5f;
+
+    private float footstepTimer;
+    private bool isInWater;
+
     private PlayerControls playerControls;
     private Animator animator;
     private Rigidbody2D rb;
@@ -20,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         playerControls = new PlayerControls();
+        audioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -62,18 +72,37 @@ public class PlayerMovement : MonoBehaviour
         Vector2 position = rb.position + movementVector * (moveSpeed * Time.fixedDeltaTime);
         rb.MovePosition(position);
         CurrentPosition = transform.position;
+
+        if (movementVector.magnitude > 0.1f)
+        {
+            PlayFootstepSound();
+        }
+    }
+
+    private void PlayFootstepSound()
+    {
+        if (footstepTimer <= 0)
+        {
+            AudioClip[] currentFootsteps = isInWater ? waterFootsteps : landFootsteps;
+            audioSource.clip = currentFootsteps[Random.Range(0, currentFootsteps.Length)];
+            audioSource.Play();
+            footstepTimer = footstepInterval;
+        }
+        footstepTimer -= Time.deltaTime;
     }
 
     private void EnterWater()
     {
         playerBoat.SetActive(true);
         animator.SetBool("InBoat", true);
+        isInWater = true;
     }
 
     private void ExitWater()
     {
         playerBoat.SetActive(false);
         animator.SetBool("InBoat", false);
+        isInWater = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
