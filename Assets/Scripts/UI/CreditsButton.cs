a4dfffaab1bc7247cf0ea5b9708a4d79e4b5b9dc
Shane Barton <shanecb@hotmail.ca>
2024-12-05 11:14:34 -0500

    Fix Menu UI scaling.

--- Assets/Scripts/UI/CreditsButton.cs
+++ Assets/Scripts/UI/CreditsButton.cs
@@ -1,15 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CreditsButton : MonoBehaviour
 {
-    private GameObject creditsUI;
-
-    private void Awake()
-    {
-        creditsUI = GameObject.FindWithTag("CreditUI");
-    }
+    [SerializeField] private GameObject creditsUI;
 
     public void OpenCredits(bool openMenu)
     {
